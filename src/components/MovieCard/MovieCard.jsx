21f83702c2a0fbd130dc3191169883cc9a55f85a
VladimirKorolev <theing1986@gmail.com>
2025-01-24 21:33:44 +0300

    Создал компонент карточка добавил стили

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,5 +1,13 @@
 function MovieCard({ card }) {
-    return <div></div>;
+    return (
+        <div className="movie-card">
+            <div className="movie-image">
+                <img src={card.img} alt={card.title} />
+                <div className="movie-rating">⭐ {card.rating}</div>
+            </div>
+            <h3 className="movie-title"></h3>
+        </div>
+    );
 }
 
 export default MovieCard;
