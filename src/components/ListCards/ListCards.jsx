cf10ac6b7119c18e5079412733fc18a4668df0e2
VladimirKorolev <theing1986@gmail.com>
2025-02-03 22:27:40 +0300

    Вынес данные отдельно

--- src/components/ListCards/ListCards.jsx
+++ src/components/ListCards/ListCards.jsx
@@ -1,56 +1,6 @@
 import styles from "./ListCards.module.css";
 import MovieCard from "../MovieCard/MovieCard";
-
-const CARDS = [
-    {
-        id: 1,
-        img: "public/cards/black-widow.png",
-        title: "Black Widow",
-        rating: 324,
-    },
-    {
-        id: 2,
-        img: "public/cards/ShangChi.png",
-        title: "Shang Chi",
-        rating: 124,
-    },
-    {
-        id: 3,
-        img: "public/cards/Loki.png",
-        title: "Loki",
-        rating: 235,
-    },
-    {
-        id: 4,
-        img: "public/cards/HowIMetYourMother.png",
-        title: "How I Met Your Mother",
-        rating: 123,
-    },
-    {
-        id: 5,
-        img: "public/cards/MoneyHeist.png",
-        title: "Money Heist",
-        rating: 8125,
-    },
-    {
-        id: 6,
-        img: "public/cards/Friends.png",
-        title: "Friends",
-        rating: 123,
-    },
-    {
-        id: 7,
-        img: "public/cards/TheBigBangTheory.png",
-        title: "The Big Bang Theory",
-        rating: 12,
-    },
-    {
-        id: 8,
-        img: "public/cards/TwoAndaHalfMen.png",
-        title: "Two And a Half Men",
-        rating: 456,
-    },
-];
+import { CARDS } from "../../data/movies-data.jsx";
 
 function ListCards() {
     return (
